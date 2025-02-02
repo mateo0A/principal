359bf33f2fd9bee74ad7ec7b85e142dd3d21ee24
Henry <hg097870pq@gmail.com>
2025-02-02 18:40:09 -0500

    darkmode

---
+++ Javascript/Darkmode.js
@@ -0,0 +1,23 @@
+let darkmode=localStorage.getItem("dark");
+
+const eneableMode=()=>{
+    document.querySelector("body").classList.add('Modedark')
+    localStorage.setItem("dark","active")
+}
+const disableMode=()=>{
+    document.querySelector("body").classList.remove('Modedark')
+    localStorage.setItem("dark",null)
+}
+
+
+let btnM=document.querySelector(".mode-lD")
+
+
+if(darkmode="active") eneableMode()
+btnM.addEventListener("click",()=>{
+    darkmode=localStorage.getItem("dark")
+
+    darkmode !== "active" ? eneableMode():disableMode()
+    console.log(1);
+    
+})
